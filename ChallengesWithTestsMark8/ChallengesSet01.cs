38a97561d09361a01d8f3e015767cf15b8cbe52a
Jake Merlin <JacobMerlin92@outlook.com>
2022-07-13 23:24:12 -0400

    first push

---
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet01
+    {
+        public bool AreTwoNumbersTheSame(int num1, int num2)
+        {
+            if(num1 == num2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double Subtract(double minuend, double subtrahend)
+        {
+           return minuend -= subtrahend;
+        }
+
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public int GetSmallestNumber(int number1, int number2)
+        {
+            
+
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else if (number1 > number2)
+            {
+                return number2;
+            }
+            else
+            {
+                return number1;
+            }
+        }
+
+        public long Multiply(long factor1, long factor2)
+        {
+            return factor1 * factor2; 
+        }
+
+        public string GetGreeting(string nameOfPerson)
+        {
+            if (!string.IsNullOrEmpty(nameOfPerson))
+            {
+                return $"Hello, {nameOfPerson}!";
+            }
+            else
+            {
+                return $"Hello!";
+            }
+        }
+        public string GetHey()
+        {
+            string hi = "HEY!";
+            return hi;
+        }
+    }
+}
